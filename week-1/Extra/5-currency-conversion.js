98c2a61022815fc48f7d294108802952bdc42dd4
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-07-06 14:11:03 +0200

    why is wrong

--- week-1/Extra/5-currency-conversion.js
+++ week-1/Extra/5-currency-conversion.js
@@ -5,18 +5,28 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
-
+function convertToUSD(price) {
+  var priceUSD = price * 1.4;
+  return priceUSD;
+}
+console.log(convertToUSD(32));
 /*
   CURRENCY FORMATTING
   ===================
   The business is now breaking into the Brazilian market
   Write a new function for converting to the Brazilian real (exchange rate is 5.7 BRL to £)
-  They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
+  They have also decided that they should add a 1% fee to all foreign transactions, which 
+  means you only convert 
+  99% of the £ to BRL.
 */
 
-function convertToBRL() {}
-
+function convertToBRL(price, fee) {
+  var bzMarket = (price * 5.7) * fee;
+  return bzMarket
+  
+}
+console.log(convertToBRL(30, 0.99));
+const { X509Certificate } = require('crypto');
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
 
